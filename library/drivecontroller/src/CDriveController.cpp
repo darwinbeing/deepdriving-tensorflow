a38c5e37649dffaf4e40acfac2d3e1bd3f96b285
Andre <andre.netzeband@hm.edu>
2017-05-27 11:08:01 +0200

    Adapted drive-controller to current car.

--- library/drivecontroller/src/CDriveController.cpp
+++ library/drivecontroller/src/CDriveController.cpp
@@ -32,6 +32,7 @@
 
 // project includes
 #include <dd/drivecontroller/CDriveController.h>
+#include <dd/common/datatypes.h>
 
 namespace dd
 {
@@ -42,13 +43,13 @@ CDriveController::CDriveController(int Lanes)
 {
   mLanes = Lanes;
   slow_down = 0;
-  pre_dist_L = 0;
-  pre_dist_R = 0;
+  pre_dist_L = 60;
+  pre_dist_R = 60;
   left_clear = 0;
   left_timer = 0;
   right_clear = 0;
   right_timer = 0;
-  timer_set = 0;
+  timer_set = 60;
   lane_change = 0;
   steer_trend = 0;
   steering_record[0] = 0;
@@ -56,7 +57,7 @@ CDriveController::CDriveController(int Lanes)
   steering_record[2] = 0;
   steering_record[3] = 0;
   steering_record[4] = 0;
-  coe_steer = 0;
+  coe_steer = 1.0;
   center_line = 0;
   pre_ML = 0;
   pre_MR = 0;
@@ -144,6 +145,8 @@ void CDriveController::controlLane1(Indicators_t const &rIndicators, Control_t &
 
 void CDriveController::controlLane2(Indicators_t const &rIndicators, Control_t &rControl)
 {
+  float const MaxSpeed      = 22;
+  float const MaxCurvySpeed = 17.5;
   slow_down=100;
 
   if (pre_dist_L<20 && rIndicators.DistLL<20) {   // left lane is occupied or not
@@ -169,14 +172,13 @@ void CDriveController::controlLane2(Indicators_t const &rIndicators, Control_t &
     right_clear=1;
   }
 
-
-  if (lane_change==0 && rIndicators.DistMM<15) {   // if current lane is occupied
+  if (lane_change==0 && rIndicators.DistMM < 25) {   // if current lane is occupied
 
     steer_trend=steering_record[0]+steering_record[1]+steering_record[2]+steering_record[3]+steering_record[4];   // am I turning or not
 
-    if (rIndicators.LL>-8 && left_clear==1 && steer_trend>=0) {   // move to left lane
+    if (rIndicators.LL>-8 && left_clear==1 && steer_trend>=0 && rIndicators.Fast == 1) {   // move to left lane
       lane_change=-2;
-      coe_steer=6;
+      coe_steer=2;
       right_clear=0;
       right_timer=0;
       left_clear=0;
@@ -184,22 +186,14 @@ void CDriveController::controlLane2(Indicators_t const &rIndicators, Control_t &
       timer_set=30;
     }
 
-    else if (rIndicators.RR<8 && right_clear==1 && steer_trend<=0) {   // move to right lane
+    else if (rIndicators.RR<8 && right_clear==1 && steer_trend<=0 && rIndicators.Fast == 1) {   // move to right lane
       lane_change=2;
-      coe_steer=6;
+      coe_steer=2;
       left_clear=0;
       left_timer=0;
       right_clear=0;
       right_timer=0;
       timer_set=30;
-    }
-
-    else {
-      double v_max=20;
-      double c=2.772;
-      double d=-0.693;
-      slow_down=v_max*(1-exp(-c/v_max*rIndicators.DistMM-d));  // optimal vilcity car-following model
-      if (slow_down<0) slow_down=0;
     }
   }
 
@@ -208,23 +202,37 @@ void CDriveController::controlLane2(Indicators_t const &rIndicators, Control_t &
 
     steer_trend=steering_record[0]+steering_record[1]+steering_record[2]+steering_record[3]+steering_record[4];  // am I turning or not
 
-    if (rIndicators.LL<-8 && right_clear==1 && steer_trend<=0 && steer_trend>-0.2) {  // in left lane, move to right lane
+    if (rIndicators.LL<-8 && right_clear==1 && steer_trend<=0 && steer_trend>-0.2 && rIndicators.Fast == 1) {  // in left lane, move to right lane
       lane_change=2;
-      coe_steer=6;
+      coe_steer=2;
       right_clear=0;
       right_timer=20;
     }
   }
   ///////////////////////////////////////////////// END prefer to stay in the right lane
 
+  if (rIndicators.DistMM < 25)
+  {
+    double v_max=MaxSpeed;
+    double c=2.772;
+    double d=-0.693;
+    slow_down=v_max*(1-exp(-c/v_max*(rIndicators.DistMM)-d));  // optimal vilocity car-following model
+    if (slow_down<0) slow_down=0;
+  }
+
+  if (rIndicators.DistMM < 10)
+  {
+    slow_down=0;
+  }
+
   ///////////////////////////////////////////////// implement lane changing or car-following
   if (lane_change==0) {
     if (-rIndicators.ML+rIndicators.MR<5.5) {
-      coe_steer=1.5;
+      coe_steer=1.0;
       center_line=(rIndicators.ML+rIndicators.MR)/2;
       pre_ML=rIndicators.ML;
       pre_MR=rIndicators.MR;
-      if (rIndicators.M<1)
+      if ((rIndicators.M < 1) && (rIndicators.M > -1))
         coe_steer=0.4;
     } else {
       if (-pre_ML>pre_MR)
@@ -238,11 +246,24 @@ void CDriveController::controlLane2(Indicators_t const &rIndicators, Control_t &
   else if (lane_change==-2) {
     if (-rIndicators.ML+rIndicators.MR<5.5) {
       center_line=(rIndicators.LL+rIndicators.ML)/2;
-      if (rIndicators.L>-5 && rIndicators.M<1.5)
-        center_line=(center_line+(rIndicators.L+rIndicators.M)/2)/2;
+      if (rIndicators.L>-5 && (rIndicators.M < 1.5) && (rIndicators.M > -1.5))
+      {
+        if (-rIndicators.L > 3)
+        {
+          center_line=(center_line+(rIndicators.L+rIndicators.M)/2)/2;
+        }
+        else
+        {
+          coe_steer  = 1;
+          if (rIndicators.M < 1.25)
+          {
+            lane_change = 0;
+          }
+        }
+      }
     } else {
       center_line=(rIndicators.L+rIndicators.M)/2;
-      coe_steer=20;
+      coe_steer=1;
       lane_change=-1;
     }
   }
@@ -261,11 +282,24 @@ void CDriveController::controlLane2(Indicators_t const &rIndicators, Control_t &
   else if (lane_change==2) {
     if (-rIndicators.ML+rIndicators.MR<5.5) {
       center_line=(rIndicators.RR+rIndicators.MR)/2;
-      if (rIndicators.R<5 && rIndicators.M<1.5)
-        center_line=(center_line+(rIndicators.R+rIndicators.M)/2)/2;
+      if (rIndicators.R<5 && (rIndicators.M < 1.5) && (rIndicators.M > -1.5))
+      {
+        if (rIndicators.R > 3)
+        {
+          center_line=(center_line+(rIndicators.R+rIndicators.M)/2)/2;
+        }
+        else
+        {
+          coe_steer  = 1;
+          if (rIndicators.M > -1.25)
+          {
+            lane_change = 0;
+          }
+        }
+      }
     } else {
       center_line=(rIndicators.R+rIndicators.M)/2;
-      coe_steer=20;
+      coe_steer=1;
       lane_change=1;
     }
   }
@@ -283,7 +317,7 @@ void CDriveController::controlLane2(Indicators_t const &rIndicators, Control_t &
   ///////////////////////////////////////////////// END implement lane changing or car-following
 
   static const float road_width = 8.0;
-  rControl.Steering = (rIndicators.Angle - center_line/road_width) / 0.541052/coe_steer;   // steering control, "rControl.Steering" [-1,1] is the value sent back to TORCS
+  rControl.Steering = (rIndicators.Angle - center_line/road_width)/coe_steer;
 
   if (lane_change==0 && coe_steer>1 && rControl.Steering>0.1)   // reshape the steering control curve
     rControl.Steering=rControl.Steering*(2.5*rControl.Steering+0.75);
@@ -293,15 +327,15 @@ void CDriveController::controlLane2(Indicators_t const &rIndicators, Control_t &
   if (steering_head==5) steering_head=0;
 
 
-  if (rIndicators.Fast==1) desired_speed=20;
-  else desired_speed=20-fabs(steering_record[0]+steering_record[1]+steering_record[2]+steering_record[3]+steering_record[4])*4.5;
+  if (rIndicators.Fast==1) desired_speed=MaxSpeed;
+  else desired_speed=MaxCurvySpeed-fabs(steering_record[0]+steering_record[1]+steering_record[2]+steering_record[3]+steering_record[4])*4.5; // reduce speed
   if (desired_speed<10) desired_speed=10;
 
   if (slow_down<desired_speed) desired_speed=slow_down;
 
   ///////////////////////////// speed control
   if (desired_speed>=rIndicators.Speed) {
-    rControl.Accelerating = 0.2*(desired_speed-rIndicators.Speed+1);
+    rControl.Accelerating = 0.1*(desired_speed-rIndicators.Speed+1);
     if (rControl.Accelerating>1) rControl.Accelerating=1.0;
     rControl.Breaking = 0.0;
   } else {
@@ -310,6 +344,15 @@ void CDriveController::controlLane2(Indicators_t const &rIndicators, Control_t &
     rControl.Accelerating = 0.0;
   }
   ///////////////////////////// END speed control
+
+  ///// Emergency Break
+  if (slow_down < 0.1)
+  {
+    rControl.Accelerating = 0.0;
+    rControl.Breaking     = 1.0;
+  }
+  /////
+
 }
 
 void CDriveController::controlLane3(Indicators_t const &rIndicators, Control_t &rControl)
