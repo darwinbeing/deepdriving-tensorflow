Fixed issues with wrong shape of labels.

python/modules/deep_driving/db/GraphDB.py
@@ -75,20 +75,20 @@ def buildFeatureParser(SerializedExample):
 
   Inputs = [
     Image,                          # 0
-    Angle,                          # 1
-    Features['Fast']*0.6 + 0.2,     # 2  - Range  0 .. 1    mapping to 0.2 .. 0.8
-    Features['LL']*0.14545+1.40909, # 3  - Range -9 .. -3.5 mapping to 0.1 .. 0.9
-    Features['ML']*0.16   +0.9,     # 4  - Range -5 .. 0    mapping to 0.1 .. 0.9
-    Features['MR']*0.16   +0.1,     # 5  - Range  0 .. 5    mapping to 0.1 .. 0.9
-    Features['RR']*0.14545-0.40909, # 6  - Range 3.5 .. 9   mapping to 0.1 .. 0.9
-    Features['DistLL']/112+0.1,     # 7  - Range  0 ..  90  mapping to 0.1 .. 0.9
-    Features['DistMM']/112+0.1,     # 8  - Range  0 ..  90  mapping to 0.1 .. 0.9
-    Features['DistRR']/112+0.1,     # 9  - Range  0 ..  90  mapping to 0.1 .. 0.9
-    Features['L']*0.17778+1.34445,  # 10 - Range -7 .. -2.5 mapping to 0.1 .. 0.9
-    Features['M']*0.1149 +0.6714,   # 11 - Range -5 ..  2   mapping to 0.1 .. 0.9
-    Features['R']*0.17778-0.34445,  # 12 - Range 2.5 .. 7   mapping to 0.1 .. 0.9
-    Features['DistL']/112+0.1,      # 13 - Range  0 ..  90  mapping to 0.1 .. 0.9
-    Features['DistR']/112+0.1,      # 14 - Range  0 ..  90  mapping to 0.1 .. 0.9
+    tf.reshape(Angle,                          shape=[1]), # 1
+    tf.reshape(Features['Fast']*0.6 + 0.2,     shape=[1]), # 2  - Range  0 .. 1    mapping to 0.2 .. 0.8
+    tf.reshape(Features['LL']*0.14545+1.40909, shape=[1]), # 3  - Range -9 .. -3.5 mapping to 0.1 .. 0.9
+    tf.reshape(Features['ML']*0.16   +0.9,     shape=[1]), # 4  - Range -5 .. 0    mapping to 0.1 .. 0.9
+    tf.reshape(Features['MR']*0.16   +0.1,     shape=[1]), # 5  - Range  0 .. 5    mapping to 0.1 .. 0.9
+    tf.reshape(Features['RR']*0.14545-0.40909, shape=[1]), # 6  - Range 3.5 .. 9   mapping to 0.1 .. 0.9
+    tf.reshape(Features['DistLL']/112+0.1,     shape=[1]), # 7  - Range  0 ..  90  mapping to 0.1 .. 0.9
+    tf.reshape(Features['DistMM']/112+0.1,     shape=[1]), # 8  - Range  0 ..  90  mapping to 0.1 .. 0.9
+    tf.reshape(Features['DistRR']/112+0.1,     shape=[1]), # 9  - Range  0 ..  90  mapping to 0.1 .. 0.9
+    tf.reshape(Features['L']*0.17778+1.34445,  shape=[1]), # 10 - Range -7 .. -2.5 mapping to 0.1 .. 0.9
+    tf.reshape(Features['M']*0.1149 +0.6714,   shape=[1]), # 11 - Range -5 ..  2   mapping to 0.1 .. 0.9
+    tf.reshape(Features['R']*0.17778-0.34445,  shape=[1]), # 12 - Range 2.5 .. 7   mapping to 0.1 .. 0.9
+    tf.reshape(Features['DistL']/112+0.1,      shape=[1]), # 13 - Range  0 ..  90  mapping to 0.1 .. 0.9
+    tf.reshape(Features['DistR']/112+0.1,      shape=[1]), # 14 - Range  0 ..  90  mapping to 0.1 .. 0.9
   ]
 
   return Inputs

python/modules/deep_driving/model/CError.py
@@ -23,9 +23,13 @@ class CError(dl.error.CMeasurement):
     with tf.name_scope("Loss"):
       print("Create Loss Function...")
 
-      SquaredLoss = tf.constant(0, dtype=tf.float32)
+      SquaredLoss = None
       for i, Out in enumerate(Output):
-        SquaredLoss = SquaredLoss + tf.nn.l2_loss(Label[i] - Output[i])
+        SingleSquaredLoss = tf.square(Label[i] - Output[i])
+        if SquaredLoss is None:
+          SquaredLoss = SingleSquaredLoss
+        else:
+          SquaredLoss = SquaredLoss + SingleSquaredLoss
 
       print("* Squared Loss shape: {}".format(SquaredLoss.shape))
 
@@ -47,17 +51,22 @@ class CError(dl.error.CMeasurement):
     with tf.name_scope("Error"):
       print("Create Mean Absolute Error Function...")
 
-      AbsoluteError = tf.constant(0, dtype=tf.float32)
+      AbsoluteError = None
       for i, Out in enumerate(Output):
-        AbsoluteError = AbsoluteError + tf.abs(Label[i] - Output[i])
-
-      AbsoluteError = tf.reshape(AbsoluteError, shape=[-1])
+        SingleError = tf.abs(Label[i] - Output[i])
+        if AbsoluteError is None:
+          AbsoluteError = SingleError
+        else:
+          AbsoluteError = AbsoluteError + SingleError
 
       print("* Absolute Error shape: {}".format(AbsoluteError.shape))
 
       MeanAbsolutError = tf.reduce_mean(AbsoluteError)
 
       Mean, Var = tf.nn.moments(AbsoluteError, axes=[0])
+
+      Mean = tf.reshape(Mean, shape=[])
+      Var  = tf.reshape(Var , shape=[])
 
       tf.summary.scalar('MAE', MeanAbsolutError)
       tf.summary.scalar('MeanAbsolutError', Mean)