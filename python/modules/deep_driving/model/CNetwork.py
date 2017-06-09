f09b968c2675ddd5564256becc440efa850920a4
Andre <andre.netzeband@hm.edu>
2017-06-09 21:15:47 +0200

    Use a 3 layer network.

--- python/modules/deep_driving/model/CNetwork.py
+++ python/modules/deep_driving/model/CNetwork.py
@@ -28,6 +28,8 @@ import tensorflow as tf
 
 class CNetwork(dl.network.CNetwork):
   def _build(self, Inputs, Settings):
+    dl.layer.setupLogger(self.log)
+
     Scope = "Network"
 
     with tf.variable_scope(Scope):
@@ -35,11 +37,20 @@ class CNetwork(dl.network.CNetwork):
 
       Input       = self._preprocessImage(Inputs['Image'])
       OutputNodes = len(Inputs['Labels'])
+      Output = Input
 
       self.log(" * network Input-Shape: {}".format(Input.shape))
 
-      Output = Input
-      Output = self._createDenseLayer(Inputs=Output, OutputSize=OutputNodes, Name="Dense_1")
+
+      # Fully Connected Layer - 1024
+      Output = dl.layer.createFullyConnected(Input=Output, Size=1024, Func="ReLU", Name="FC_1")
+
+      # Fully Connected Layer - 256
+      Output = dl.layer.createFullyConnected(Input=Output, Size=256, Func="ReLU", Name="FC_2")
+
+      # Output Layer - 14
+      Output = dl.layer.createDense(Input=Output, Size=OutputNodes, Name="Dense_3")
+
 
       self.log(" * network Output-Shape: {}".format(Output.shape))
 
@@ -64,32 +75,16 @@ class CNetwork(dl.network.CNetwork):
 
 ## Custom methods
 
-  def _createDenseLayer(self, Inputs, OutputSize, Name="Dense"):
-    OutputSize = int(OutputSize)
-    self.log(" * Create Dense-Layer \"{}\" with {} output-nodes.".format(Name, OutputSize))
-    InputShape = Inputs.shape
-    if len(InputShape) > 2:
-      InputLength = int(np.prod(InputShape[1:]))
-      self.log("   * Reshape layer input {} to vector with {} elements.".format(InputShape, InputLength))
-      Inputs = tf.reshape(Inputs, shape=[-1, InputLength])
-    with tf.name_scope(Name):
-      X       = tf.identity(Inputs, name="X")
-      Weights = tf.Variable(initial_value=tf.random_normal(shape=[int(X.shape[1]), OutputSize], mean=0, stddev=0.1), name = "W")
-      Bias    = tf.Variable(initial_value=tf.random_normal(shape=[OutputSize],                  mean=0, stddev=0.1), name = "b")
-      Signal  = tf.add(tf.matmul(X, Weights), Bias, name="s")
-      return Signal
+#  def _createActivation(self, Signal, Activation = tf.identity, Name="Activation"):
+#    with tf.name_scope(Name):
+#      Y       = Activation(Signal, name="y")
+#      return Y
 
 
-  def _createActivation(self, Signal, Activation = tf.identity, Name="Activation"):
-    with tf.name_scope(Name):
-      Y       = Activation(Signal, name="y")
-      return Y
-
-
-  def _createFullyConnectedLayer(self, Inputs, OutputSize, Name="FullyConnected"):
-    with tf.name_scope(Name):
-      Signal = self._createDenseLayer(Inputs, OutputSize, Name="Dense")
-      return self._createActivation(Signal, tf.nn.relu, Name="Activation")
+#  def _createFullyConnectedLayer(self, Inputs, OutputSize, Name="FullyConnected"):
+#    with tf.name_scope(Name):
+#      Signal = self._createDenseLayer(Inputs, OutputSize, Name="Dense")
+#      return self._createActivation(Signal, tf.nn.relu, Name="Activation")
 
 
   def _preprocessImage(self, Image, Name = "Preprocessing"):
@@ -97,8 +92,3 @@ class CNetwork(dl.network.CNetwork):
       self.log("* Preprocess Image by adding -0.5")
       Image = Image - 0.5
       return Image
-
-
-  def _reshapeImageToVector(self, Image, Name="Image"):
-    VectorSize = int(np.prod(Image.shape[1:]))
-    return tf.reshape(Image, shape=[-1, VectorSize], name=Name)
