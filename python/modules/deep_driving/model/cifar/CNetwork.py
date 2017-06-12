e368f72d042e6fcace8628f2a15e1cb7ecd5a907
Andre <andre.netzeband@hm.edu>
2017-06-12 11:51:50 +0200

    Added network from Tensorflow Cifar tutorial.

--- python/modules/deep_driving/model/cifar/CNetwork.py
+++ python/modules/deep_driving/model/cifar/CNetwork.py
@@ -25,6 +25,7 @@ import deep_learning as dl
 import numpy as np
 import tensorflow as tf
 
+from .TutorialHelper import _activation_summary, _variable_with_weight_decay, _variable_on_cpu, FLAGS
 
 class CNetwork(dl.network.CNetwork):
   def _build(self, Inputs, Settings):
@@ -45,11 +46,7 @@ class CNetwork(dl.network.CNetwork):
 
       self.log(" * network Input-Shape: {}".format(Input.shape))
 
-      # Output Layer
-      with tf.name_scope("Output"):
-        OutputNodes = 10
-        Output = dl.layer.createDense(Input=Output, Size=OutputNodes)
-
+      Output = self._buildNetwork(Input)
 
       self.log(" * network Output-Shape: {}".format(Output.shape))
 
@@ -74,3 +71,81 @@ class CNetwork(dl.network.CNetwork):
       #Image -= 0.5
       #Image = dl.layer.createBatchNormalization(Input=Image)
       return Image
+
+  def _buildNetwork(self, images):
+
+    # We instantiate all variables using tf.get_variable() instead of
+    # tf.Variable() in order to share variables across multiple GPU training runs.
+    # If we only ran this model on a single GPU, we could simplify this function
+    # by replacing all instances of tf.get_variable() with tf.Variable().
+    #
+    # conv1
+    with tf.variable_scope('conv1') as scope:
+      kernel = _variable_with_weight_decay('weights',
+                                           shape=[5, 5, 3, 64],
+                                           stddev=5e-2,
+                                           wd=0.0)
+      conv = tf.nn.conv2d(images, kernel, [1, 1, 1, 1], padding='SAME')
+      biases = _variable_on_cpu('biases', [64], tf.constant_initializer(0.0))
+      pre_activation = tf.nn.bias_add(conv, biases)
+      conv1 = tf.nn.relu(pre_activation, name=scope.name)
+      _activation_summary(conv1)
+
+    # pool1
+    pool1 = tf.nn.max_pool(conv1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME', name='pool1')
+
+    # norm1
+    norm1 = tf.nn.lrn(pool1, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75, name='norm1')
+
+    # conv2
+    with tf.variable_scope('conv2') as scope:
+      kernel = _variable_with_weight_decay('weights',
+                                           shape=[5, 5, 64, 64],
+                                           stddev=5e-2,
+                                           wd=0.0)
+      conv = tf.nn.conv2d(norm1, kernel, [1, 1, 1, 1], padding='SAME')
+      biases = _variable_on_cpu('biases', [64], tf.constant_initializer(0.1))
+      pre_activation = tf.nn.bias_add(conv, biases)
+      conv2 = tf.nn.relu(pre_activation, name=scope.name)
+      _activation_summary(conv2)
+
+    # norm2
+    norm2 = tf.nn.lrn(conv2, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75,
+                      name='norm2')
+    # pool2
+    pool2 = tf.nn.max_pool(norm2, ksize=[1, 3, 3, 1],
+                           strides=[1, 2, 2, 1], padding='SAME', name='pool2')
+
+    # local3
+    with tf.variable_scope('local3') as scope:
+      # Move everything into depth so we can perform a single matrix multiply.
+      reshape = tf.reshape(pool2, [FLAGS.batch_size, -1])
+      dim = reshape.get_shape()[1].value
+      weights = _variable_with_weight_decay('weights', shape=[dim, 384],
+                                            stddev=0.04, wd=0.004)
+      biases = _variable_on_cpu('biases', [384], tf.constant_initializer(0.1))
+      local3 = tf.nn.relu(tf.matmul(reshape, weights) + biases, name=scope.name)
+      _activation_summary(local3)
+
+    # local4
+    with tf.variable_scope('local4') as scope:
+      weights = _variable_with_weight_decay('weights', shape=[384, 192],
+                                            stddev=0.04, wd=0.004)
+      biases = _variable_on_cpu('biases', [192], tf.constant_initializer(0.1))
+      local4 = tf.nn.relu(tf.matmul(local3, weights) + biases, name=scope.name)
+      _activation_summary(local4)
+
+    NUM_CLASSES = 10
+    # linear layer(WX + b),
+    # We don't apply softmax here because
+    # tf.nn.sparse_softmax_cross_entropy_with_logits accepts the unscaled logits
+    # and performs the softmax internally for efficiency.
+    with tf.variable_scope('softmax_linear') as scope:
+      weights = _variable_with_weight_decay('weights', [192, NUM_CLASSES],
+                                              stddev=1 / 192.0, wd=0.0)
+      biases = _variable_on_cpu('biases', [NUM_CLASSES],
+                                  tf.constant_initializer(0.0))
+      softmax_linear = tf.add(tf.matmul(local4, weights), biases, name=scope.name)
+      _activation_summary(softmax_linear)
+
+    return softmax_linear
---
+++ python/modules/deep_driving/model/cifar/TutorialHelper.py
@@ -0,0 +1,87 @@
+# The MIT license:
+#
+# Copyright 2017 Andre Netzeband
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
+# to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of 
+# the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO 
+# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+# SOFTWARE.
+#
+# Note: The DeepDriving project on this repository is derived from the DeepDriving project devloped by the princeton 
+# university (http://deepdriving.cs.princeton.edu/). The above license only applies to the parts of the code, which 
+# were not a derivative of the original DeepDriving project. For the derived parts, the original license and 
+# copyright is still valid. Keep this in mind, when using code from this project.
+
+import tensorflow as tf
+import re
+
+FLAGS = tf.app.flags.FLAGS
+
+TOWER_NAME = 'tower'
+
+# Basic model parameters.
+tf.app.flags.DEFINE_integer('batch_size', 64, """Number of images to process in a batch.""")
+tf.app.flags.DEFINE_boolean('use_fp16', False,"""Train the model using fp16.""")
+
+def _activation_summary(x):
+  """Helper to create summaries for activations.
+  Creates a summary that provides a histogram of activations.
+  Creates a summary that measures the sparsity of activations.
+  Args:
+    x: Tensor
+  Returns:
+    nothing
+  """
+  # Remove 'tower_[0-9]/' from the name in case this is a multi-GPU training
+  # session. This helps the clarity of presentation on tensorboard.
+  tensor_name = re.sub('%s_[0-9]*/' % TOWER_NAME, '', x.op.name)
+  tf.summary.histogram(tensor_name + '/activations', x)
+  tf.summary.scalar(tensor_name + '/sparsity', tf.nn.zero_fraction(x))
+
+
+def _variable_on_cpu(name, shape, initializer):
+  """Helper to create a Variable stored on CPU memory.
+  Args:
+    name: name of the variable
+    shape: list of ints
+    initializer: initializer for Variable
+  Returns:
+    Variable Tensor
+  """
+  with tf.device('/cpu:0'):
+    dtype = tf.float16 if FLAGS.use_fp16 else tf.float32
+    var = tf.get_variable(name, shape, initializer=initializer, dtype=dtype)
+  return var
+
+
+def _variable_with_weight_decay(name, shape, stddev, wd):
+  """Helper to create an initialized Variable with weight decay.
+  Note that the Variable is initialized with a truncated normal distribution.
+  A weight decay is added only if one is specified.
+  Args:
+    name: name of the variable
+    shape: list of ints
+    stddev: standard deviation of a truncated Gaussian
+    wd: add L2Loss weight decay multiplied by this float. If None, weight
+        decay is not added for this Variable.
+  Returns:
+    Variable Tensor
+  """
+  dtype = tf.float16 if FLAGS.use_fp16 else tf.float32
+  var = _variable_on_cpu(
+      name,
+      shape,
+      tf.truncated_normal_initializer(stddev=stddev, dtype=dtype))
+  if wd is not None:
+    weight_decay = tf.multiply(tf.nn.l2_loss(var), wd, name='weight_loss')
+    tf.add_to_collection('losses', weight_decay)
+  return var
