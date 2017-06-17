bb1e7f33597a75f996542f53e5cdf80b59700abf
Andre <andre.netzeband@hm.edu>
2017-06-17 10:36:50 +0200

    Disable data augmentation.

--- python/modules/deep_driving/model/CReader.py
+++ python/modules/deep_driving/model/CReader.py
@@ -104,23 +104,24 @@ class CReader(dl.data.CReader):
         Image = Inputs[0]
 
         if IsTraining:
-          print("* Perform data-augmentation")
+          #print("* Perform data-augmentation")
 
-          Image = tf.image.random_brightness(Image, max_delta=0.25)
+          #Image = tf.image.random_brightness(Image, max_delta=0.25)
 
-          Image = tf.image.random_contrast(Image, lower=0.75, upper=1.25)
+          #Image = tf.image.random_contrast(Image, lower=0.75, upper=1.25)
 
-          Image = tf.image.random_saturation(Image, lower=0.75, upper=1.25)
+          #Image = tf.image.random_saturation(Image, lower=0.75, upper=1.25)
 
-          Image = tf.image.random_hue(Image, max_delta=0.1)
+          #Image = tf.image.random_hue(Image, max_delta=0.1)
+          pass
 
         print("* Perform per-pixel standardization")
 
         MeanImage = tf.image.resize_images(MeanReader.MeanImage, size=(int(Image.shape[0]), int(Image.shape[1])))
-        VarImage = tf.image.resize_images(MeanReader.VarImage, size=(int(Image.shape[0]), int(Image.shape[1])))
+        #VarImage = tf.image.resize_images(MeanReader.VarImage, size=(int(Image.shape[0]), int(Image.shape[1])))
 
         Image = tf.subtract(Image, MeanImage)
-        Image = tf.div(Image, tf.sqrt(VarImage))
+        #Image = tf.div(Image, tf.sqrt(VarImage))
 
         Inputs[0] = Image
 
