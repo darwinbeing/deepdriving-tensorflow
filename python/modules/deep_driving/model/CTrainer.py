5ae93686fc93535f348112f3a3f0403a90d9308d
Andre <andre.netzeband@hm.edu>
2017-06-20 15:26:48 +0200

    Use AdaDelta Optimizer with learning rate 1.0 (run_9).

--- python/modules/deep_driving/model/CTrainer.py
+++ python/modules/deep_driving/model/CTrainer.py
@@ -25,7 +25,8 @@ class CTrainer(dl.trainer.CTrainer):
       tf.summary.scalar("Step", CurrentOptimizationStep)
 
       #Optimizer = tf.train.AdamOptimizer(learning_rate=LearnRate)
-      Optimizer = tf.train.MomentumOptimizer(learning_rate=LearnRate, momentum=Settings['Optimizer']['Momentum'], use_nesterov = True)
+      Optimizer = tf.train.AdadeltaOptimizer(learning_rate=1.0)
+      #Optimizer = tf.train.MomentumOptimizer(learning_rate=LearnRate, momentum=Settings['Optimizer']['Momentum'], use_nesterov = True)
       Gradients = Optimizer.compute_gradients(ErrorMeasurement.getOutputs()['Loss'])
 
       print("Apply individual learning rate scales...")
